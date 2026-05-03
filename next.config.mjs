92436cc57f69cbdbf201eae9b6c01bf156275019
Nihal Kumar <knihal03@gmail.com>
2026-05-03 19:32:42 -0400

    security

--- next.config.mjs
+++ next.config.mjs
@@ -12,12 +12,6 @@ const nextConfig = {
       },
     ],
   },
-};
-
-export default nextConfig;
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
   async redirects() {
     return [
       {
